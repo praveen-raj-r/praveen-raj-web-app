ef570e3b10b9d8be27971514d5d0b32b4d31888e
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-09-05 15:01:43 +0530

    added footer and navbar

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function AppLayout() {
   return (
-    <div>
-      App Layout
-      <Outlet />
-    </div>
+    <main className="relative h-screen z-[1] bg-[#0A0A0D] main-container text-white">
+      <div className="max-w-[708px] mx-auto pt-6 ">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </main>
   );
 }
 
