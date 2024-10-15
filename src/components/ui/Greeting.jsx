81239446405a3b62693002d07563898c2e18b137
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-15 21:50:01 +0530

    update

--- src/components/ui/Greeting.jsx
+++ src/components/ui/Greeting.jsx
@@ -3,18 +3,19 @@ import chat from "/chat.png";
 function Greeting() {
   return (
     <section className="flex flex-col-reverse items-center gap-10 p-4 mx-5 md:gap-0 md:flex-row greetings-card">
-      <div className="z-10 p-2 md:p-8">
+      <div className="p-2 md:p-8">
         <h1 className="gradient retro-2 text-4xl md:text-5xl leading-[-1px] font-bold text-center md:text-left">
           Say hi!
         </h1>
 
-        <div className="flex flex-col mt-3 space-x-0 text-center md:space-x-6 md:flex-row">
-          <a className="gradient retro-2" href="">
+        <div className="flex flex-col mt-3 space-x-0 text-lg font-thin tracking-wider text-center sm:text-left md:space-x-6 md:flex-row">
+          <a
+            className="text-white gradient retro-2"
+            href="mailto:praveen1220raj@gmail.com"
+          >
             praveen1220raj@gmail.com
           </a>
-          <a className="gradient retro-2" href="">
-            @realPraveen
-          </a>
+          <a className="gradient retro-2">@realPraveen</a>
         </div>
 
         <p className="sm:text-lg font-normal sm:leading-[120%] my-5 mx-0 tracking-wide opacity-80 max-w-[340px]">
@@ -25,10 +26,7 @@ function Greeting() {
         <div className="flex justify-start m-0 text-left">
           <ul className="flex ">
             <li className="m-0">
-              <a
-                href=""
-                className="rounded-[18px] flex items-center font-medium text-base leading-5 relative text-white py-2 px-5 design-pur"
-              >
+              <a className="rounded-[18px] flex items-center font-medium text-base leading-5 relative text-white py-2 px-5 design-pur">
                 Send Work Proposal
               </a>
             </li>
@@ -36,8 +34,8 @@ function Greeting() {
         </div>
       </div>
 
-      <div className="z-10 text-right">
-        <img className="chatImg h-[220px] md:h-[320px]" src={chat} />
+      <div className="text-right">
+        <img className="chatImg h-[220px] md:h-auto" src={chat} />
       </div>
     </section>
   );
