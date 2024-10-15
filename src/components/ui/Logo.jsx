a4beeef237ff27fc52a96119701b06e53d409481
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-15 19:52:16 +0530

    updated navbar

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import whitelogo from "/realPraveen-logo-white.svg";
 
 function Logo() {
   return (
-    <div className="flex items-center max-h-10 p-0 scale-[0.92]">
+    <div className="flex items-center max-h-10 p-0 scale-[0.92] ml-[-30px]">
       <Link to="/">
         <img className="" src={whitelogo} />
       </Link>
