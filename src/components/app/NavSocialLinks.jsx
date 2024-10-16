e4f3d6992999d72edeca1c1b7a66cbcd7784461e
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-16 09:10:46 +0530

    added sdiebar

--- src/components/app/NavSocialLinks.jsx
+++ src/components/app/NavSocialLinks.jsx
@@ -1,31 +1,78 @@
-import linkedIn from "/linkedin.svg";
-import github from "/github.svg";
-import darkMoon from "/moon-dark-mode.svg";
+// import linkedIn from "/linkedin.svg";
+// import github from "/github.svg";
+// import darkMoon from "/moon-dark-mode.svg";
+import { Github, Linkedin, Menu, Moon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const size = 18;
+
+const contents = [
+  {
+    icon: <Linkedin size={size} />,
+    link: "https://www.linkedin.com/in/developer-praveen-raj/",
+  },
+  {
+    icon: <Github size={size} />,
+    link: "https://github.com/praveen-raj-r",
+    rightBorder: true,
+  },
+  {
+    icon: <Moon size={size} />,
+  },
+  {
+    icon: <Menu size={size} />,
+    toggleSideBar: true,
+  },
+];
 
 function NavSocialLinks() {
   return (
-    <div className="flex items-center mr-4 max-h-10">
-      <div className="flex items-center max-h-10 border-r border-[#ffffff1a] mr-3">
-        <a
-          target="_blank"
-          className="leading-[0px] py-[2px] px-3"
-          href="https://www.linkedin.com/in/developer-praveen-raj/"
-        >
-          <img src={linkedIn} />
-        </a>
-        <a
-          target="_blank"
-          className="leading-[0px] py-[2px] px-3"
-          href="https://github.com/praveen-raj-r"
-        >
-          <img src={github} />
-        </a>
-      </div>
-      <div className="flex items-center max-h-10">
-        <img src={darkMoon} />
-      </div>
+    <div className="flex items-center gap-4">
+      {contents.map((item) => (
+        <NavIcon key={item} item={item}></NavIcon>
+      ))}
     </div>
   );
+}
+
+function NavIcon({ item }) {
+  if (item.toggleSideBar)
+    return (
+      <Sheet>
+        <SheetTrigger>
+          <span>{item.icon}</span>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    );
+  if (item.link)
+    return (
+      <span
+        className={`${
+          item.rightBorder ? "border-r-2 border-[#ffffff1a] pr-2" : ""
+        }`}
+      >
+        <a target="_blank" href={item.link}>
+          {item.icon}
+        </a>
+      </span>
+    );
+  return <span>{item.icon}</span>;
 }
 
 export default NavSocialLinks;
