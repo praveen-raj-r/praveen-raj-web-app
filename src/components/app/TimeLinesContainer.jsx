279f8a9d7d72fbe89e67f7123d61e647272e62e1
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2024-10-18 10:28:03 +0530

    updated timeline

--- src/components/app/TimeLinesContainer.jsx
+++ src/components/app/TimeLinesContainer.jsx
@@ -1,50 +1,6 @@
-const timelines = [
-  {
-    range: "2015 - 2016",
-    label: [
-      { text: "education", class: "yellow" },
-      { text: "SSLC", class: "green" },
-    ],
-    title: "SSLC",
-    desc: "Studied @ Swami Vivekananda Maticulation Hr. Sec. School and Secured 91%",
-    img: "",
-    class: "yellow",
-  },
-  {
-    range: "2017 - 2018",
-    label: [
-      { text: "education", class: "yellow" },
-      { text: "HSC", class: "green" },
-    ],
-    title: "HSC",
-    desc: "Studied @ Swami Vivekananda Maticulation Hr. Sec. School and Secured 60.75%",
-    img: "",
-    class: "purple",
-  },
-  {
-    range: "2018 - 2022",
-    label: [
-      { text: "education", class: "yellow" },
-      { text: "graduate", class: "green" },
-    ],
-    title: "B.E. Electrical And Electronics Engineering",
-    desc: "Grudated from Meenakshi College Of Engineering and Secured 8.15 CGPA",
-    img: "",
-    class: "yellow",
-  },
-  {
-    range: "May 2023 - present",
-    label: [{ text: "work", class: "orange" }],
-    title: "LCS Controls Private Limited.",
-    desc: "Working as a Frontend Engineer",
-    img: "",
-    class: "green",
-  },
-];
-
-function TimeLinesContainer() {
+function TimeLinesContainer({ timelines }) {
   return (
-    <div className=" pt-14">
+    <div className="flex flex-col-reverse pt-14">
       {timelines.map((item, i) => (
         <TimeLineItem item={item} key={`timeline-${i}`} />
       ))}
@@ -55,11 +11,11 @@ function TimeLinesContainer() {
 function TimeLineItem({ item }) {
   return (
     <div className="relative w-full mx-auto my-0 cursor-pointer">
-      <a className="flex justify-center" href="">
+      <a className="flex justify-center px-2" href="">
         <div className="min-w-[100px] max-w-[100px] mr-5 -mt-1">
           <p className="text-sm font-semibold text-right">{item.range}</p>
         </div>
-        <div className="w-[420px] relative pl-4 pb-12 border-l border-[#ecedee26] design-dot">
+        <div className="w-auto sm:w-[420px] relative pl-4 pb-12 border-l border-[#ecedee26] design-dot">
           <div className="relative">
             <div className="flex flex-col p-3 -translate-y-5 design-hover-before">
               <div>
--- src/pages/TimeLine.jsx
+++ src/pages/TimeLine.jsx
@@ -5,49 +5,92 @@ import { Instagram, Linkedin } from "lucide-react";
 const size = 15;
 
 const socialLinksContent = [
-    {
-        icon: <Linkedin size={size} />,
-        label: "LinkedIn",
-        class: "orange",
-        link: "https://www.linkedin.com/in/developer-praveen-raj/",
-    },
-    {
-        icon: <Instagram size={size} />,
-        label: "Instagram",
-        class: "purple",
-        link: "https://www.instagram.com/real_praveenraj/https://www.instagram.com/real_praveenraj/;",
-    },
+  {
+    icon: <Linkedin size={size} />,
+    label: "LinkedIn",
+    class: "orange",
+    link: "https://www.linkedin.com/in/developer-praveen-raj/",
+  },
+  {
+    icon: <Instagram size={size} />,
+    label: "Instagram",
+    class: "purple",
+    link: "https://www.instagram.com/real_praveenraj/https://www.instagram.com/real_praveenraj/;",
+  },
+];
+const timelines = [
+  {
+    range: "2015 - 2016",
+    label: [
+      { text: "education", class: "yellow" },
+      { text: "SSLC", class: "green" },
+    ],
+    title: "SSLC",
+    desc: "Studied @ Swami Vivekananda Maticulation Hr. Sec. School and Secured 91%",
+    img: "",
+    class: "yellow",
+  },
+  {
+    range: "2017 - 2018",
+    label: [
+      { text: "education", class: "yellow" },
+      { text: "HSC", class: "green" },
+    ],
+    title: "HSC",
+    desc: "Studied @ Swami Vivekananda Maticulation Hr. Sec. School and Secured 60.75%",
+    img: "",
+    class: "purple",
+  },
+  {
+    range: "2018 - 2022",
+    label: [
+      { text: "education", class: "yellow" },
+      { text: "graduate", class: "green" },
+    ],
+    title: "B.E. Electrical And Electronics Engineering",
+    desc: "Grudated from Meenakshi College Of Engineering and Secured 8.15 CGPA",
+    img: "",
+    class: "yellow",
+  },
+  {
+    range: "May 2023 - present",
+    label: [{ text: "work", class: "orange" }],
+    title: "LCS Controls Private Limited.",
+    desc: "Working as a Frontend Engineer",
+    img: "",
+    class: "green",
+  },
 ];
 
 function TimeLine() {
-    return (
-        <>
-            <Description header="Timeline">
-                The linear view of some milestones and notable moments that happened so
-                far (127 key moments). And you can always find more information on
-                LinkedIn.
-            </Description>
-            <div className="flex justify-center gap-10">
-                {socialLinksContent.map((item, i) => (
-                    <SocialLinks item={item} key={`social-links-${i}`} />
-                ))}
-            </div>
-            <TimeLinesContainer />
-        </>
-    );
+  return (
+    <>
+      <Description header="Timeline">
+        The linear view of some milestones and notable moments that happened so
+        far ({timelines.length} key moments). And you can always find more
+        information on LinkedIn.
+      </Description>
+      <div className="flex justify-center gap-10">
+        {socialLinksContent.map((item, i) => (
+          <SocialLinks item={item} key={`social-links-${i}`} />
+        ))}
+      </div>
+      <TimeLinesContainer timelines={timelines} />
+    </>
+  );
 }
 
 function SocialLinks({ item }) {
-    return (
-        <a
-            target="_blank"
-            href={item.link}
-            className={`flex items-center gap-1 px-3 py-1.5 font-semibold text-base rounded-2xl ${item.class}`}
-        >
-            {item.icon}
-            {item.label}
-        </a>
-    );
+  return (
+    <a
+      target="_blank"
+      href={item.link}
+      className={`flex items-center gap-1 px-3 py-1.5 font-semibold text-base rounded-2xl ${item.class}`}
+    >
+      {item.icon}
+      {item.label}
+    </a>
+  );
 }
 
 export default TimeLine;
