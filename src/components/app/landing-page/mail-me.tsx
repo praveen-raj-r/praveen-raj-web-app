23c7a35dcd27b12d5fae846212a4052a51a76a24
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-04-02 14:49:58 +0530

    updated ui

--- src/components/app/landing-page/mail-me.tsx
+++ src/components/app/landing-page/mail-me.tsx
@@ -1,15 +1,13 @@
-import { AnimatedGradientText } from "@/components/magicui/animated-gradient-text";
 import { AuroraText } from "@/components/magicui/aurora-text";
-import { cn } from "@/lib/utils";
-import { ChevronRight } from "lucide-react";
+import SayHi from "./say-hi";
 
 const MailMe = () => {
   return (
     <div className="my-10 mt-20 flex flex-col items-center gap-5">
-      <p className="gradient sky-2 text-2xl">Get in Touch</p>
+      <p className="gradient sky-2 text-3xl">Get in Touch</p>
 
-      <h1 className="text-3xl sm:text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl text-center">
-        <AuroraText>Let’s Work Together</AuroraText>
+      <h1 className="text-3xl sm:text-4xl font-bold md:tracking-tighter md:text-5xl lg:text-7xl text-center">
+        <AuroraText className="leading-[140%]">Let’s Work Together</AuroraText>
       </h1>
 
       <p className="text-center text-lg sm:text-2xl">
@@ -18,29 +16,7 @@ const MailMe = () => {
         want to say hi, I’ll try my best to get back to you!.
       </p>
 
-      <a href="mailto:praveen1220raj@gmail.com">
-        <div className="group relative mx-auto flex items-center justify-center rounded-full px-4 py-1.5 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] ">
-          <span
-            className={cn(
-              "absolute inset-0 block h-full w-full animate-gradient rounded-[inherit] bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:300%_100%] p-[1px]"
-            )}
-            style={{
-              WebkitMask:
-                "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
-              WebkitMaskComposite: "destination-out",
-              mask: "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
-              maskComposite: "subtract",
-              WebkitClipPath: "padding-box",
-            }}
-          />
-          🎉
-          <hr className="mx-2 h-4 w-px shrink-0 bg-neutral-500" />
-          <AnimatedGradientText className="text-sm font-medium">
-            Say Hi!
-          </AnimatedGradientText>
-          <ChevronRight className="ml-1 size-4 stroke-neutral-500 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-        </div>
-      </a>
+      <SayHi />
     </div>
   );
 };
--- src/components/app/landing-page/profile.tsx
+++ src/components/app/landing-page/profile.tsx
@@ -2,7 +2,7 @@ import ProfileImg from "./profile-img";
 
 const Profile = () => {
   return (
-    <h2 className="text-4xl sm:text-[50px] md:text-[56px] font-light tracking-[-1.5px] md:tracking-[2.5px] sm:leading-[44px] leading-[34px] md:leading-[54px] max-w-[600px] font-Fraunces mt-10 sm:mt-16">
+    <h2 className="text-4xl sm:text-[50px] md:text-[56px] font-light tracking-[-1.5px] md:tracking-[2.5px] sm:leading-[44px] leading-[34px] md:leading-[54px] sm:max-w-[600px] font-Fraunces mt-10 sm:mt-16">
       Hi, I'm
       <ProfileImg />
       Praveen Raj
