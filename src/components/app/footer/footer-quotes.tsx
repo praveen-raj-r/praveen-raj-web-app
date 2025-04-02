8036766d2d15debe85f21482dc9de7f886bd269d
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-04-02 15:00:18 +0530

    updated mobile spacing

--- src/components/app/footer/footer-quotes.tsx
+++ src/components/app/footer/footer-quotes.tsx
@@ -1,6 +1,6 @@
 const FooterQuotes = () => {
     return (
-        <p className="my-10 text-xl text-gray-100 gradient sky-2 p-4">
+        <p className="my-5 sm:my-10 text-xl text-gray-100 gradient sky-2 p-4">
             Build using{" "}
             <a className="gradient retro-2" href="https://vite.dev/" target="_blank">
                 Vite
--- src/components/app/landing-page/mail-me.tsx
+++ src/components/app/landing-page/mail-me.tsx
@@ -3,14 +3,14 @@ import SayHi from "./say-hi";
 
 const MailMe = () => {
   return (
-    <div className="my-10 mt-20 flex flex-col items-center gap-5">
+    <div className="sm:my-10 mt-20 flex flex-col items-center gap-5">
       <p className="gradient sky-2 text-3xl">Get in Touch</p>
 
       <h1 className="text-3xl sm:text-4xl font-bold md:tracking-tighter md:text-5xl lg:text-7xl text-center">
         <AuroraText className="leading-[140%]">Let’s Work Together</AuroraText>
       </h1>
 
-      <p className="text-center text-lg sm:text-2xl">
+      <p className="text-center text-lg sm:text-xl">
         I’m open for new opportunities – especially ambitious or large projects.
         However, my inbox is always open. Whether you have a question or just
         want to say hi, I’ll try my best to get back to you!.
