09e24bb637a0b0ae6c570646b50bf11c3d2e77d4
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2025-04-03 15:51:07 +0530

    added project links and images

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,47 +1,19 @@
 export const projects = [
     {
-        id: "project-1",
-        img: "/evergreen-escapes.png",
-        header: "Evergreen Escapes",
-        description: "Beautifully Crafted Cabin Booking Website",
-        hrefUrl: "https://evergreen-escapes.vercel.app/",
+      id: "project-1",
+      img: "/projects/taskFlow.png",
+      header: "Task Flow",
+      description:
+        "A powerful task management app designed for seamless workflow automation.",
+      hrefUrl: "https://task-flow-cloud.vercel.app/",
     },
     {
-        id: "project-2",
-        img: "/shortenr.png",
-        header: "Shortenr App",
-        description: "The only URL Shortener you'll ever need! ",
-        hrefUrl: "https://shortenr-app.vercel.app/",
+      id: "project-2",
+      img: "/projects/weatherly.png",
+      header: "Weatherly",
+      description:
+        "Real-time weather updates, location-based forecasts, and beautiful UI design",
+      hrefUrl: "https://weatherly-cloud.vercel.app/",
     },
-    {
-        id: "project-3",
-        img: "/eventify-logo.png",
-        header: "Eventify",
-        description: "Simplify Your Events Scheduling",
-        hrefUrl: "https://eventify-web-app.vercel.app/",
-    },
-    {
-        id: "project-4",
-        img: "/elevora-logo.png",
-        header: "Elevora",
-        description:
-            "Explore thousands of job listings or find the perfect candidate",
-        hrefUrl: "https://elevora.vercel.app/",
-    },
-    {
-        id: "project-5",
-        img: "/sprinto-logo.png",
-        header: "Sprinto",
-        description:
-            "Empower your team with our intuitive project management solution.",
-        hrefUrl: "https://elevora.vercel.app/",
-    },
-    {
-        id: "project-6",
-        img: "/skyviewer.png",
-        header: "SkyViewer",
-        description:
-            "Weather app provides real-time forecasts, alerts, and location-based updates.",
-        hrefUrl: "https://sky-viewer.vercel.app/",
-    },
-];
+  ];
+  
