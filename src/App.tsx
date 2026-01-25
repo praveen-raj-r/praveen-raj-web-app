0b54c4a01014d65c31c95190233caf83333579f9
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2026-01-25 22:14:37 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/context/theme-provider";
 import PageNotFound from "./pages/page-not-found";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/landing-page";
+import { Analytics } from "@vercel/analytics/react";
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -15,7 +16,9 @@ const routes = createBrowserRouter([
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={routes} /> <Toaster />
+      <RouterProvider router={routes} />
+      <Toaster />
+      <Analytics />
     </ThemeProvider>
   );
 }
