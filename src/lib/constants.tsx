bee180395ef7f6cdc1a7c4c741576c35136aefac
praveenraj <105029450+praveen-raj-r@users.noreply.github.com>
2026-01-25 21:32:45 +0530

    cleared version1 code

--- src/lib/constants.tsx
+++
@@ -1,4 +0,0 @@
-export const linkedUrl = "https://www.linkedin.com/in/developer-praveen-raj/";
-export const instagramUrl = "https://www.instagram.com/real_praveenraj/";
-export const githubUrl = "https://github.com/praveen-raj-r";
-export const resumePdfUrl = "resume.pdf";
